f20d0339de035c11ba87a1e407ce490a9ca0749c
miho <z16012mu@aiit.ac.jp>
2016-06-13 00:04:15 +0900

    add test

--- kadai-test.bash
+++ kadai-test.bash
@@ -15,9 +15,27 @@ diff $tmp-ans $tmp-out || ERROR_EXIT "TEST1-DIFF"
 rm -f $tmp-*
 
 #TEST2
-echo 2 > $tmp-ans
+echo 4 > $tmp-ans
 ./16445112 320 12 > $tmp-out || ERROR_EXIT "TEST2-BASH"
 diff $tmp-ans $tmp-out || ERROR_EXIT "TEST2-DIFF"
+rm -f $tmp-*
+
+#TEST3
+echo 100 > $tmp-ans
+./16445112 100 100 > $tmp-out || ERROR_EXIT "TEST3-BASH"
+diff $tmp-ans $tmo-out || ERROR_EXIT "TEST3-DIFF"
+rm -f $tmp-*
+
+#TEST4
+echo 引数を正しく入力してください > $tmp-ans
+./16445112 > $tmp-out || ERROR_EXIT "TEST4-BASH"
+diff $tmp-ans $tmp-out || ERROR_EXIT "TEST4-DIFF"
+rm -f $tmp-*
+
+#TEST5
+echo 引数を正しく入力してください > $tmp-ans
+./16445112 > $tmp-out || ERROR_EXIT "TEST5-BASH"
+diff $tmp-ans $tmp-out || ERROR_EXIT "TEST5-DIFF"
 rm -f $tmp-*
 
 #TEST END
