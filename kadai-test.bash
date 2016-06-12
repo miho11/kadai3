6863f8bc75e7281da8d1840ee91d989148c81fee
miho <z16012mu@aiit.ac.jp>
2016-06-12 23:44:01 +0900

    add test

--- kadai-test.bash
+++ kadai-test.bash
@@ -2,14 +2,23 @@
 
 tmp=/tmp/$$
 
-#ERROR_EXIT () {
-#  echo "$1" > &2
-#  rm -f $tmp-*
-#  exit 1
-#}
+ERROR_EXIT () {
+  echo "$1" >&2
+  rm -f $tmp-*
+  exit 1
+}
 
 #TEST1
-./16445112 10 5
+echo 5 > $tmp-ans
+./16445112 10 5 > $tmp-out || ERROR_EXIT "TEST1-BASH"
+diff $tmp-ans $tmp-out || ERROR_EXIT "TEST1-DIFF"
+rm -f $tmp-*
+
+#TEST2
+echo 2 > $tmp-ans
+./16445112 320 12 > $tmp-out || ERROR_EXIT "TEST2-BASH"
+diff $tmp-ans $tmp-out || ERROR_EXIT "TEST2-DIFF"
+rm -f $tmp-*
 
 #TEST END
 exit 0
